950d2f00f771a7732d88ae99d554a2a1a0429607
thiagodutra <thiagodutra@gmail.com>
2018-03-07 10:38:38 -0300

    Added comments to the code

--- src/bfs/BredthFirstSearch.java
+++ src/bfs/BredthFirstSearch.java
@@ -1,5 +1,33 @@
 package bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**This Algorithm searches a graph 'horizontally' first**/
 public class BredthFirstSearch {
+	
+	public void bfs( Vertex root) {
+		/**Creates a queue of linked lists**/
+		Queue<Vertex> queue = new LinkedList<>();
+		/**Updates de root as visited and adds it to the queue**/
+		root.setVisited(true);
+		queue.add(root);
+		/**Walk through the entire queue**/
+		while (!queue.isEmpty()) {
+			/**Updates the actual vertex, removes it from queue then print it**/
+			Vertex actualVertex = queue.remove();
+			System.out.println(actualVertex+"-");
+			/**Iterates through the actual vertex neighbor's list**/
+			for (Vertex v : actualVertex.getNeighborList()) {
+				/**Sets Vertex v as visited and adds it to the queue**/
+				if (!v.isVisited()) {
+					v.setVisited(true);
+					queue.add(v);
+				}
+				
+			}
+			
+		}
+	}
 
 }
--- src/bfs/Vertex.java
+++ src/bfs/Vertex.java
@@ -2,17 +2,59 @@ package bfs;
 
 import java.util.ArrayList;
 import java.util.List;
-
+	/**Vertex Class**/
 public class Vertex {
-
+	
+	
+	/**Attributes: data 
+	 * 			  visited boolean - stores if it was visited or not
+	 * 			  List<vertex> stores the vertex neighbors **/
 	private int data;
 	private boolean visited;
-	private List<Vertex> neighbourList;
+	private List<Vertex> neighborList;
 	
+	/**Constructor: Already and data and creates a neighbour's list**/
 	public Vertex(int data) {
 		this.data = data;
-		this.neighbourList = new ArrayList<>();
+		this.neighborList = new ArrayList<>();
 		
 	}
+	/**Adds a Vertex as a neighbor **/
+	public void addNeighbor(Vertex vertex) {
+		this.neighborList.add(vertex);
+	}
+	
+	/** Getters and Setters**/
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public List<Vertex> getNeighborList() {
+		return neighborList;
+	}
+
+	public void setNeighborList(List<Vertex> neighborList) {
+		this.neighborList = neighborList;
+	}
+	
+	/** Rewriting the toString method**/
+	@Override
+	public String toString() {
+		return ""+this.data;
+	}
+	
+	
 	
 }
